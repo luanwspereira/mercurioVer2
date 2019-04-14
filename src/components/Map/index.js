7aefc1975c5abaa39bf41b50bdd3107eb8aa5bf7
Luwesipe <luwesipe@gmail.com>
2019-04-14 18:17:49 -0300

    SearchBar and Routes
    Added stylized search bar and routes.

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import MapView from 'react-native-maps';
 import { PermissionsAndroid } from 'react-native';
 import { View } from 'react-native';
+import Search from '../Search';
+import Directions from '../Directions';
+import { getPixelSize } from '../utils';
 
 export default class Map extends Component {
     state ={
         region: null,
+        destination: null,
     };
 
     async componentDidMount(){
@@ -29,8 +33,19 @@ export default class Map extends Component {
         );
     }
     
+    handleLocationSelected = (data, { geometry }) => {
+        const { location: { lat: latitude, lng: longitude } } = geometry;
+        this.setState({
+            destination: {
+                latitude,
+                longitude,
+                title: data.structured_formatting.main_text,
+            }
+        })
+    } 
+
     render(){
-        const { region } = this.state;
+        const { region, destination } = this.state;
         return(
             <View style={{flex:1}}>
                 <MapView
@@ -44,6 +59,28 @@ export default class Map extends Component {
                 region={region}
                 
                 loadingEnabled
+                ref = {el => (this.mapView = el)}
+                >
+                    {destination && (
+                        <Directions
+                            origin={region}
+                            destination = {destination}
+                            onReady={result => {
+                                this.mapView.fitToCoordinates(result.coordinates, {
+                                    edgePadding: {
+                                        right: getPixelSize(50),
+                                        left: getPixelSize(50),
+                                        bottom: getPixelSize(50),
+                                        top:getPixelSize(50)
+                                    }
+                                });
+                            }}
+                        />
+                    )}
+
+                </MapView>
+                <Search 
+                onLocationSelected= {this.handleLocationSelected}
                 />
 
             </View>
