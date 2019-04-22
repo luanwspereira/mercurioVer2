a7e269d1a726fce6ba9d2bf03a5ee8c5dc929538
Luwesipe <luwesipe@gmail.com>
2019-04-22 19:29:31 -0300

    morto

--- src/configs/statusbar.js
+++
@@ -1,4 +0,0 @@
-import {StatusBar} from 'react-native';
-
-StatusBar.setBackgroundColor('#6D3BD2');
-StatusBar.setBarStyle("light-content");
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Routes from './routes';
-import "./configs/statusbar";
-const App = () => <Routes />
-
-
-
-export default App;
